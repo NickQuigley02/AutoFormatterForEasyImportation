d3c31a936f6917d85ffe0806f6585187a31afaba
Nicholas Quigley <112343257+NickQuigley02@users.noreply.github.com>
2022-11-01 16:30:49 -0500

    CHANGES text.txt to calInput.txt
    This is more descriptive.

--- w2wFormat.sh
+++ w2wFormat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-awk '/SDG/ {print}' text.txt | cut -d "-" -f 1 | tr -d " " > formattedsubject.txt
-awk '/to/ {print}' text.txt | cut -d " " -f 6 | tr -d " " > time.txt
-awk '/to/ {print}' text.txt | cut -d " " -f 2-4 > date.txt
+awk '/SDG/ {print}' calInput.txt | cut -d "-" -f 1 | tr -d " " > formattedsubject.txt
+awk '/to/ {print}' calInput.txt | cut -d " " -f 6 | tr -d " " > time.txt
+awk '/to/ {print}' calInput.txt | cut -d " " -f 2-4 > date.txt
 
 date "+%T" --file=time.txt > formattedtime.txt
 date "+%D" --file=date.txt > formatteddate.txt
